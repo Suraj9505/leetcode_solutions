46aef1c626fb4ef6385066b1e304a92ae20c3073
Suraj Jha <sjha5505@gmail.com>
2024-08-20 20:32:53 +0530

    2sum solution

---
+++ 2Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sums;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    sums.push_back(i);
+                    sums.push_back(j);
+                    return sums;
+            }
+        }
+    }
+    return sums;
+    }
+};
